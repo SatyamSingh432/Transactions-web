2b08463b08e2bba9a6158cb88f3f08fc58b6dad4
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-24 22:40:52 +0530

    feat:add form

---
+++ src/components/Form.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Form = () => {
+  return (
+    
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="bg-black text-white" variant="outline">
+            + Add Expense
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Save Transaction</DialogTitle>
+            {/* <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription> */}
+          </DialogHeader>
+          <form
+      onSubmit={() => {
+        console.log("satyam");
+      }}
+    >
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Amount
+              </Label>
+              <Input
+                type="number"
+                id="name"
+                //   value="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Date
+              </Label>
+              <Input
+                type="date"
+                id="username"
+                //   value="@peduarte"
+                className="col-span-3"
+              />
+            </div>
+            <Textarea placeholder="Description..." />
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </form>
+  );
+};
+
+export default Form;
