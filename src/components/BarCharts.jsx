7a2880d80f783c26ad315368a11f17fbb05c6223
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-24 22:40:29 +0530

    feat:add bar chart

---
+++ src/components/BarCharts.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis } from "recharts";
+
+const data = [
+  {
+    name: "JAN",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "FEB",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "MAR",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "JUN",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "JUL",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "AUG",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "SEP",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "OCT",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "NOV",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "DEC",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page K",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Page L",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const BarCharts = () => {
+  return (
+    <>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={50} height={40} data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
+
+export default BarCharts;
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { Button } from "./ui/button";
 const TransactionList = () => {
   return (
-    <div className=" grid grid-cols-[1fr_2fr_1fr] px-4 py-2 text-xs ">
-      <div className="rounded-lg flex justify-center flex-col ">
+    <div className=" grid grid-cols-[1fr_2fr_1fr] px-4 py-2 text-xs gap-1 ">
+      <div className="rounded-lg flex justify-start gap-4 items-center ">
         <div>
-          <span className="text-base font-semibold">Amount:</span> 30$
+          <span className="text-sm font-semibold">Amount:</span> 30$
         </div>
         <div>
-          <span className="text-base font-semibold">Date:</span>01/03/2025
+          <span className="text-sm font-semibold">Date:</span>01/03/2025
         </div>
       </div>
-      <div className="">
-        <span className="text-base font-semibold">Decription:</span> Lorem ipsum
+      <div className="flex items-center">
+        <span className="text-sm font-semibold">Decription:</span> Lorem ipsum
         dolor sit amet consectetur adipisicing elit. Ipsa ab minima, dicta,
         voluptates molestiae
       </div>
       <div className="flex justify-center gap-4 items-center ">
-        <Button className="text-sm ">EDIT</Button>
+        <Button className="text-sm  ">EDIT</Button>
         <Button className="text-sm ">DELETE</Button>
       </div>
       <hr className="col-span-3  border-gray-400" />
