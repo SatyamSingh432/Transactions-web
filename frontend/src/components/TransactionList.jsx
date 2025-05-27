2046e3c907b639305fc64cc59235596fbb23c773
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-27 16:45:55 +0530

    fix:fix responsiveness

--- frontend/src/components/TransactionList.jsx
+++ frontend/src/components/TransactionList.jsx
@@ -21,18 +21,17 @@ const TransactionList = ({
     }
   };
   return (
-    <div className="grid  xl:grid-cols-[1fr_2fr_0.5fr] lg:grid-cols-[1fr_3fr] px-4 py-1 text-xs lg:gap-y-1 md:gap-y-2 ">
+    <div className="grid grid-cols-2 xl:grid-cols-[1fr_2fr_0.5fr] sm:grid-cols-[1fr_3fr] md:grid-cols-[1fr_3fr] lg:grid-cols-[1fr_3fr] px-4 py-1 text-xs lg:gap-y-1 md:gap-y-2 sm:gap-y-2">
       <div className="rounded-lg grid  justify-start gap-1  ">
         <div>
-          <span className="text-sm font-semibold ">Amount:</span>{" "}
+          <span className="text-sm font-semibold ">Amount :</span>{" "}
           {`${amount} $`}
         </div>
         <div>
-          <span className="text-sm font-semibold">Date:</span>
-          {`${date}`}
+          <span className="text-sm font-semibold">Date :</span> {`${date}`}
         </div>
         <div>
-          <span className="text-sm font-semibold">Category:</span>
+          <span className="text-sm font-semibold">Category : </span>{" "}
           {`${category}`}
         </div>
       </div>
@@ -40,7 +39,7 @@ const TransactionList = ({
         <span className="text-sm font-semibold pr-2">Decription:</span>{" "}
         {`${description}`}
       </div>
-      <div className="flex justify-center  gap-4 items-center sm:col-span-3 md:col-span-2 lg:col-span-1 pb-1">
+      <div className="flex justify-center  gap-4 items-center col-span-2 sm:col-span-2 md:col-span-2 lg:col-span-3 xl:col-span-1 pb-2">
         {/* <Button className="text-sm  ">EDIT</Button>
          */}
         <EditForm
@@ -54,8 +53,8 @@ const TransactionList = ({
           setExpData={setExpData}
           category={category}
         />
-        <Button className="text-sm cursor-pointer " onClick={deleteHandler}>
-          DELETE
+        <Button className="text-xs cursor-pointer " onClick={deleteHandler}>
+          Delete
         </Button>
       </div>
       <hr className="col-span-3  border-gray-400 md:col-span-2  lg:col-span-3" />
