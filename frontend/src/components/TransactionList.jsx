8805578aa3303ca92205bbe1cdd935e4e3186d91
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-27 12:47:59 +0530

    fix: fix responsiveness

--- frontend/src/components/TransactionList.jsx
+++ frontend/src/components/TransactionList.jsx
@@ -21,8 +21,8 @@ const TransactionList = ({
     }
   };
   return (
-    <div className="grid md:grid-cols-1 lg:grid-cols-[1.4fr_1fr_0.5fr] px-4 py-1 text-xs lg:gap-y-1 md:gap-y-2 ">
-      <div className="rounded-lg grid grid-cols-1  justify-start gap-4 items-center ">
+    <div className="grid  xl:grid-cols-[1fr_2fr_0.5fr] lg:grid-cols-[1fr_3fr] px-4 py-1 text-xs lg:gap-y-1 md:gap-y-2 ">
+      <div className="rounded-lg grid  justify-start gap-1  ">
         <div>
           <span className="text-sm font-semibold ">Amount:</span>{" "}
           {`${amount} $`}
@@ -36,11 +36,11 @@ const TransactionList = ({
           {`${category}`}
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="flex sm:col-span-2 md:col-span-1">
         <span className="text-sm font-semibold pr-2">Decription:</span>{" "}
         {`${description}`}
       </div>
-      <div className="flex justify-center gap-4 items-center md:col-span-2 lg:col-span-1">
+      <div className="flex justify-center  gap-4 items-center sm:col-span-3 md:col-span-2 lg:col-span-1 pb-1">
         {/* <Button className="text-sm  ">EDIT</Button>
          */}
         <EditForm
@@ -58,7 +58,7 @@ const TransactionList = ({
           DELETE
         </Button>
       </div>
-      <hr className="col-span-3  border-gray-400 md:col-span-2 lg:col-span-3" />
+      <hr className="col-span-3  border-gray-400 md:col-span-2  lg:col-span-3" />
     </div>
   );
 };
